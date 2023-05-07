113feb1af27171f67dc6f766aaca12ed3f4c3c09
wasifmohammad6@gmail.com <Administrator@Tanveer-47735.wgbd.com>
2023-05-07 17:28:51 +0600

    Third commit

--- ecomm/urls.py
+++ ecomm/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
 
     path('accounts/', include('accounts.urls') ),
     path('admin/', admin.site.urls),
+    path('products/', include('products.urls')),
+    path('',  include('home.urls')),
 ]
 if settings.DEBUG:
         urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
